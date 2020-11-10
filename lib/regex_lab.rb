e862090e2f9562f2d50e60db0e7e7f170b4e8d09
Benjamin Cheng <bencheng200@gmail.com>
2020-11-10 20:50:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word =~ /\b[aeiouAEIOU]/
+    return true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return arr = text.scan(\bun\w+ing\b)
 end
 
 def words_five_letters_long(text)
