ce91ec8bcb9b7e95951291660894480df1bdf9ea
Benjamin Cheng <bencheng200@gmail.com>
2020-11-10 21:17:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone =~ /\d{3}\d{3}\d{4}/
+    return true
+  else 
+    return false
 end
