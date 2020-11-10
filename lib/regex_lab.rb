f985e21032e7d7f0e12796ad2cdc2b1c73692e64
Benjamin Cheng <bencheng200@gmail.com>
2020-11-10 21:28:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone =~ /^[2-9]\d{2}(-| )\d{3}(-| )\d{4}$/
+  if phone =~ /[2-9]\d{2}(-| )\d{3}(-| )\d{4}/
     return true
   else
     return false
